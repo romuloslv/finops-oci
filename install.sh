c5ba22c4bd10004cd02b50d0ec4cd6f7deb2a12c
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2022-04-05 20:52:56 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\nexport OCI_CLI_AUTH=instance_principal" >> ~/.bashrc
-yum -y update && sudo yum -y upgrade
-yum install -y deltarpm python36-oci-cli
-timedatectl set-timezone America/Sao_Paulo
-crontab schedule.cron
